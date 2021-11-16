15b4c9cf02cb4d86793f1bec072a759e40545daf
Peyman Zarchini <zarchinipeyman213@gmail.com>
2021-11-17 01:05:55 +0330

    Fix UI

--- js/script.js
+++ js/script.js
@@ -1,31 +1,32 @@
-function convertSeconds(s) {
-    let sec = Math.floor(s % 60);
-    let min = Math.floor(s / 60);
-    let hour;
-    if (min >= 60) {
-        hour = Math.floor(min / 60);
-        min %= 60;
-        return nf(hour, 2) + ":" + nf(min, 2) + ":" + nf(sec, 2);
-    }
-    return nf("0", 2) + ":" + nf(min, 2) + ":" + nf(sec, 2);
-}
+function countDownTimer() {
+    let countDownDate = new Date("Nov 17, 2050 24:00:00").getTime();
+    console.log(countDownDate);
 
-function setup() {
-    let counter = 0;
-    let timeLeft = 90000;
-    let timer = document.querySelectorAll(".timer span");
-    for (let i = 0; i < timer.length; i++) {
-        timer[i].innerHTML = convertSeconds(timeLeft - counter);
-        let interval = setInterval(timeIt, 1000);
-        function timeIt() {
-            counter++;
-            timer[i].innerHTML = convertSeconds(timeLeft - counter);
-            if (counter === timeLeft) {
-                counter = 0;
-                clearInterval(interval);
-            }
+    let m = setInterval(function () {
+        let now = new Date().getTime();
+        let distance = countDownDate - now;
+
+        let hours = Math.floor(
+            (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        let timer = document.querySelectorAll(".timer span");
+
+        for (let i = 0; i < timer.length; i++) {
+            timer[i].innerHTML = `${hours}:${minutes}:${seconds}`;
         }
-    }
+
+        if (distance < 0) {
+            clearInterval(m);
+            timer[i].innerHTML = "EXPIRED";
+        }
+    }, 1000);
 }
 
-setup();
+countDownTimer();
